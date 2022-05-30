d7b8c1a80b8f9de2769d39d6b5953f23860667bd
Rudra Patel <patelrudra219@gmail.com>
2022-05-30 18:04:00 +0530

    initial commit

---
+++ First/src/First.java
@@ -0,0 +1,10 @@
+import java.applet.*;
+import java.awt.Graphics;
+
+public class First extends Applet{
+
+    public void paint(Graphics g) {
+        g.drawString("Hello world!", 50, 25);
+    }
+
+}
